02a9bd7b9b59a92ec9748192e77901bd3a3ff793
Aluno <aluno@eteclencois.com.br>
2019-03-28 11:42:47 -0300

    Criação de relacionamento entre as tabelas categoria e produto

--- src/main/java/br/com/nice/aulaapi/domain/Produto.java
+++ src/main/java/br/com/nice/aulaapi/domain/Produto.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Produto implements Serializable {
@@ -18,33 +20,56 @@ public class Produto implements Serializable {
 	private Integer id;
 	private String nome;
 	private BigDecimal preco;
+	
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
+	private Categoria categoria;
 	public Produto() {
 		
 	}
-	public Produto(Integer id, String nome, BigDecimal preco) {
+
+	public Produto(Integer id, String nome, BigDecimal preco, Categoria categoria) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+		this.categoria = categoria;
 	}
+	
+	
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public BigDecimal getPreco() {
 		return preco;
 	}
+
 	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
 	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
